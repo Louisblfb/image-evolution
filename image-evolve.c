1acb96dfd49a2a70eae7f33b1f41b1bd759954ae
Louis Belford <louisblfd@gmail.com>
2014-05-25 09:30:24 +1000

    fixed indent

--- image-evolve.c
+++ image-evolve.c
@@ -11,6 +11,10 @@
 #define point_cnt   6
 #define ABS(val) ((val) < 0 ? -(val) : (val))
 
+#define CLAM(val, min, max) ((val) < (min) ? (min) : \
+                            (val) > (max) ? (max) : (val)) 
+
+
 /* global variables */
 cairo_surface_t *goal_img;
 cairo_t *goal_con;
@@ -48,9 +52,10 @@ void mutate_points();
 void mutate_XY();
 
 int 
-main(int argc, char *argv[]) {
-
-    if(argc == 1) {
+main(int argc, char *argv[]) 
+{
+    if(argc == 1) 
+    {
         printf("error: missing argument\n");
         return 0;
     }
@@ -84,8 +89,8 @@ main(int argc, char *argv[]) {
 }
 
 void 
-copy_surface(cairo_surface_t *surf, cairo_t *contxt) {
-
+copy_surface(cairo_surface_t *surf, cairo_t *contxt) 
+{
     cairo_set_source_rgb(contxt, 1,1,1);
     cairo_rectangle(contxt, 0,0, width, height);
     cairo_fill(contxt);
@@ -94,10 +99,12 @@ copy_surface(cairo_surface_t *surf, cairo_t *contxt) {
 }
 
 void
-main_loop(int argc) {
+main_loop(int argc) 
+{
     int cycle =0, check = 0;
     float best_fit;
     float evol_fit;
+    
     cairo_set_source_rgb(evol_con, 1,1,1);
     cairo_rectangle(evol_con, 0,0, width, height);
     cairo_fill(evol_con);
@@ -109,44 +116,47 @@ main_loop(int argc) {
     pert = get_fitness(goal_img, goal_img);
     cairo_surface_write_to_png(evol_img, "initial.png");
 
-    while(cycle < argc) {
+    while(cycle < argc) 
+    {
         int dec_poly = (int)((rand() / (double)RAND_MAX) * (shape_cnt));
         int dec_op = (int)((rand() / (double)RAND_MAX) * (4));
-        //printf("poly: %d - operation: %d - ", dec_poly, dec_op);
-       
+        
         if (dec_op == 0) 
             mutate_color(dna_evol, dec_poly);
         else if (dec_op == 1)
             mutate_points(dna_evol, dec_poly);
         else if (dec_op == 2)
             mutate_XY(dna_evol, dec_poly);
-        else if (dec_op == 3) {
+
+        else if (dec_op == 3) 
+        {
             int ran_poly = (int)((rand() / (double)RAND_MAX) * (shape_cnt));
             poly temp = dna_evol[dec_poly];
             dna_evol[dec_poly] = dna_evol[ran_poly];
             dna_evol[ran_poly] = temp;
-            //printf("poly: %d to poly: %d\n", dec_poly, ran_poly);
         }
         
         draw_poly(dna_evol, evol_con, evol_img); 
         evol_fit = get_fitness(goal_img, evol_img);
 
-        if (evol_fit < best_fit) {
+        if (evol_fit < best_fit) 
+        {
             printf("cycle: %d/%d fitness: %0.6f%% poly: %d\n", cycle, argc, diff, dec_poly);
             copy_dna(dna_evol, dna_best);
             best_fit = evol_fit;
         }
     
-        else if (check == 100) {
-            printf("cycle: %d\n", cycle);
+        else if (check == 100) 
+        {
+            printf("cycle: %d/%d\n", cycle, argc);
             check = 0;
         }        
 
         else 
             copy_dna(dna_best, dna_evol);
        
-        if (check == 100)
-            check = 0; 
+        if (check == 100) check = 0; 
+        
         cycle++;
         check++;
     }
@@ -155,16 +165,19 @@ main_loop(int argc) {
 }
 
 void
-seed_poly(poly dna[]) {
-
-    /* initial seed of all dna attributes */
+seed_poly(poly dna[]) 
+{
     srand(getpid() + time(NULL));
-    for (int i=0; i<shape_cnt; i++) {
-        for (int j=0; j<point_cnt; j++) {
+    
+    for (int i=0; i<shape_cnt; i++) 
+    {
+        for (int j=0; j<point_cnt; j++) 
+        {
             dna[i].pointX[j] = (float)rand() / (float)RAND_MAX;
             dna[i].pointY[j] = (float)rand() / (float)RAND_MAX;
             /*printf("point %d: %f ", j, dna[i].pointX[j]);*/
         }
+
         dna[i].x = (float)rand() / (float)RAND_MAX; 
         dna[i].y = (float)rand() / (float)RAND_MAX; 
         dna[i].r = (float)rand() / (float)RAND_MAX; 
@@ -177,100 +190,115 @@ seed_poly(poly dna[]) {
 }
 
 void 
-draw_poly(poly dna[], cairo_t *contxt, cairo_surface_t *surf) {
-
+draw_poly(poly dna[], cairo_t *contxt, cairo_surface_t *surf) 
+{
     /* draw dna to surface */
-    //cairo_set_source_rgb(contxt, 1,1,1);
-    //cairo_rectangle(contxt, 0,0, width,height);
-    //cairo_fill(contxt);
+    cairo_set_source_rgb(contxt, 1,1,1);
+    cairo_rectangle(contxt, 0,0, width,height);
+    cairo_fill(contxt);
 
     cairo_set_line_width(contxt, 0);
-    for (int i=0; i<shape_cnt; i++) {
+    
+    for (int i=0; i<shape_cnt; i++) 
+    {
+        
         cairo_set_source_rgba(contxt, dna[i].r,dna[i].g,dna[i].b,dna[i].a);
         cairo_move_to(contxt, dna[i].x, dna[i].y);
-        for (int j=0; j<point_cnt; j++) {
+        
+        for (int j=0; j<point_cnt; j++) 
             cairo_line_to(contxt, dna[i].pointX[j], dna[i].pointY[j]);
-        }
+        
         cairo_fill(contxt);
     }
 }
 
 void
-mutate_color(poly dna[], int i) {
+mutate_color(poly dna[], int i) 
+{
+    int dec = (int)((rand() / (double)RAND_MAX) * (3)); 
 
-    //srand(time(NULL));
-    dna[i].r = (float)rand() / (float)RAND_MAX; 
-    dna[i].g = (float)rand() / (float)RAND_MAX; 
-    dna[i].b = (float)rand() / (float)RAND_MAX; 
-    dna[i].a = (float)rand() / (float)RAND_MAX; 
+    if (dec == 0)
+        dna[i].r = (float)rand() / (float)RAND_MAX; 
+    
+    else if (dec == 1)
+        dna[i].g = (float)rand() / (float)RAND_MAX; 
+    
+    else if (dec == 2)
+        dna[i].b = (float)rand() / (float)RAND_MAX; 
+    
+    else if (dec == 3)
+        dna[i].a = (float)rand() / (float)RAND_MAX; 
   
 }
 
 void
-mutate_points(poly dna[], int i) {
-
-    //srand(time(NULL));
-    for (int j=0; j<point_cnt; j++) {
+mutate_points(poly dna[], int i) 
+{
+    for (int j=0; j<point_cnt; j++) 
+    {
         dna[i].pointX[j] = (float)rand() / (float)RAND_MAX;
         dna[i].pointY[j] = (float)rand() / (float)RAND_MAX;
     }
 }
 
 void 
-mutate_XY(poly dna[], int i) {
-
-    //srand(time(NULL));
+mutate_XY(poly dna[], int i) 
+{
     dna[i].x = (float)rand() / (float)RAND_MAX; 
     dna[i].y = (float)rand() / (float)RAND_MAX; 
 }
 
 float 
-get_fitness(cairo_surface_t *surface_a, cairo_surface_t *surface_b) {
-
+get_fitness(cairo_surface_t *surface_a, 
+            cairo_surface_t *surface_b) 
+{
     unsigned char *goal_surf = cairo_image_surface_get_data(surface_a);
     unsigned char *test_surf = cairo_image_surface_get_data(surface_b);
 
     float max_fitness;
     int fitness;
 
-    for (int y=0; y<height; y++) {
-        for (int x=0; x<width; x++) {                        
+    for (int y=0; y<height; y++) 
+    {
+        for (int x=0; x<width; x++) 
+        {                        
             int pixel = y*width*4 + x*4;
             
             unsigned char goal_a = goal_surf[pixel];
             unsigned char goal_r = goal_surf[pixel+1];
             unsigned char goal_g = goal_surf[pixel+2];
             unsigned char goal_b = goal_surf[pixel+3];
-
             unsigned char test_a = test_surf[pixel];
             unsigned char test_r = test_surf[pixel+1];
             unsigned char test_g = test_surf[pixel+2];
             unsigned char test_b = test_surf[pixel+3];
 
-            max_fitness += goal_a + goal_r + goal_g + goal_b;
-            
+            max_fitness += goal_a + goal_r + goal_g + goal_b;           
             fitness += ABS(test_a - goal_a);
             fitness += ABS(test_r - goal_r);
             fitness += ABS(test_g - goal_g);
             fitness += ABS(test_b - goal_b);
 
-            }
+        }
     }
      
     diff = ((max_fitness-fitness) / (float)max_fitness) * 100;
     //printf("fitness: %0.6f%%\n", ((max_fitness-fitness)/(float)max_fitness) * 100 );
-
     return fitness;
 }
 
 void
-copy_dna(poly dna_a[], poly dna_b[]) {
+copy_dna(poly dna_a[], poly dna_b[]) 
+{
 
-    for (int i=0; i<shape_cnt; i++) {
-        for (int j=0; j<point_cnt; j++) {
+    for (int i=0; i<shape_cnt; i++) 
+    {
+        for (int j=0; j<point_cnt; j++) 
+        {
             dna_b[i].pointX[j] = dna_a[i].pointX[j];
             dna_b[i].pointY[j] = dna_a[i].pointY[j];
         }
+
         dna_b[i].x = dna_a[i].x;
         dna_b[i].y = dna_a[i].y;
         dna_b[i].r = dna_a[i].r;
